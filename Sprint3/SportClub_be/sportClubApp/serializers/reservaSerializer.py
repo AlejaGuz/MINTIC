ad4777753588b53ff388cd0c2b1e5ae7302d9a91
rubendmon <rubendavidmnoared@gmail.com>
2021-10-15 13:06:22 -0500

    cambios en los serializers

--- Sprint3/SportClub_be/sportClubApp/serializers/reservaSerializer.py
+++ Sprint3/SportClub_be/sportClubApp/serializers/reservaSerializer.py
@@ -9,42 +9,6 @@ from sportClubApp.serializers.ctr_act_horSerializer import CtrActHorSerializer
 
 
 class ReservaSerializer(serializers.ModelSerializer):
-
-    clase = CtrActHorSerializer()
-    usuario = UsuarioSerializer()
-
     class Meta:
         model = Reserva
         fields = ['id_reserva', 'usuario','clase','fecha','valor','estado']
-
-    def create(self, validated_data):
-        vData = validated_data.pop('usuario')
-        vData = validated_data.pop('clase')
-
-        usuarioInstance = Usuario.objects.create(**validated_data)
-        claseInstance = CtrActHor.objects.create(**validated_data)
-
-        reservaInstance=Reserva.objects.create(usuario=usuarioInstance,clase=claseInstance, **vData)
-
-        return reservaInstance
-
-    def to_representation(self, obj):
-        usuario = Usuario.objects.get(id_usuario=obj.usuario)
-        clase = CtrActHor.objects.get(id_clase=obj.clase)
-        return {
-            'id': obj.id_reserva,
-            'usuario': {
-                'id_usuario': usuario.id_usuario,
-                'username': usuario.username,
-                'password': usuario.password,
-                'email': usuario.email,
-                'tipo': usuario.tipo,
-                'plan': usuario.plan
-                },
-            'clase': {
-                'id_clase': clase.id_clase,
-                'centro': clase.centro,
-                'actividad': clase.actividad,
-                'horario' : clase.horario
-                }
-         }
