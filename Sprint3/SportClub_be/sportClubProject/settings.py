0a7fe0a65d9ec5741132149ca748239cbf15a772
alejaguz30 <alejaguz30@gmail.com>
2021-10-21 14:36:59 -0500

    Configuración CORS en Settings

--- Sprint3/SportClub_be/sportClubProject/settings.py
+++ Sprint3/SportClub_be/sportClubProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xz#dy=y_+p(!s8d81de9!#j)u(xabw-ppsvb1y900^v6nn_&j-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sportclub-be.herokuapp.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
