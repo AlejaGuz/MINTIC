59f90bf818a7d996881ed70b86935987db2de405
alejaguz30 <alejaguz30@gmail.com>
2021-10-20 19:38:26 -0500

    deployment changes

---
+++ sportClubProject/urls.py
@@ -0,0 +1,32 @@
+"""sportClubProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from sportClubApp import views
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('user/', views.UsuarioCreateView.as_view()),
+    path('user/<int:pk>/', views.UsuarioDetailView.as_view()),
+    path('actividad/', views.ActividadCreateView.as_view()),
+    path('horario/', views.HorarioCreateView.as_view()),
+    path('fusion/', views.CtrActHorCreateView.as_view()),
+    path('reserva/', views.ReservaCreateView.as_view()),
+    path('fusionread/<int:pk>/', views.CtrActHorDetailView.as_view()),
+    path('fusionupdate/<int:pk>/', views.CtrActHorUpdateView.as_view()),
+    path('fusiondelete/<int:pk>/', views.CtrActHorDeleteView.as_view()),
+]
