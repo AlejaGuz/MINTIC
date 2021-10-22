7daea4aea1db933cc1ddbbd8c3cc8d157f92a2eb
alejaguz30 <alejaguz30@gmail.com>
2021-10-22 18:54:42 -0500

    cambios en el serializador fusion

--- Sprint3/SportClub_be/sportClubApp/serializers/ctr_act_horSerializer.py
+++ Sprint3/SportClub_be/sportClubApp/serializers/ctr_act_horSerializer.py
@@ -3,6 +3,7 @@ from sportClubApp.models.ctr_act_hor import CtrActHor
 from sportClubApp.models.horario import Horario
 from rest_framework import serializers
 from sportClubApp.models.usuario import Usuario
+from sportClubApp.models.actividad import Actividad
 from sportClubApp.serializers.actividadSerializer import ActividadSerializer
 from sportClubApp.serializers.horarioSerializer import HorarioSerializer
 from sportClubApp.serializers.usuarioSerializer import UsuarioSerializer
@@ -13,3 +14,19 @@ class CtrActHorSerializer(serializers.ModelSerializer):
     class Meta:
         model = CtrActHor
         fields = ['id_clase', 'centro','actividad','horario']
+    
+    def to_representation(self, instance):
+       
+        actividad = Actividad.objects.get(id_actividad=instance.actividad.id_actividad)
+        horario = Horario.objects.get(id_horario=instance.horario.id_horario)
+        return {
+            'actividad': {
+                'nombre' : actividad.nombre,
+                'capacidad': actividad.capacidad
+            },
+
+            'horario':{
+                'dia': horario.dia,
+                'hora': horario.hora
+            }
+        }
